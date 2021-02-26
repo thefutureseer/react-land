1f397ec611778fa2e1ea3a0b1479cedbbd71d230
Thefutureseer <danielharrislimos@outlook.com>
2021-02-25 22:42:40 -0800

    Create Aside.js

---
+++ react-land/src/Aside.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+
+
+export default class Aside extends React.Component {
+  render() {
+    return <div className="aside-div"><h1>Aside</h1></div>
+  }
+}
