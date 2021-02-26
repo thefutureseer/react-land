1d7cc4f5b72a7bc6e92f58afe6f2a666fcf80b24
Thefutureseer <danielharrislimos@outlook.com>
2021-02-25 22:45:30 -0800

    Create Section.js

---
+++ react-land/src/Section.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+
+export default class Section extends React.Component {
+  render() {
+    return <div className="section-div">
+      <h1>Section</h1>
+      <article className="article"> article </article>
+      <h1> h1 </h1>
+      <h2> h2 </h2>
+      <h3> h3 </h3>
+      </div>
+  }
+}
