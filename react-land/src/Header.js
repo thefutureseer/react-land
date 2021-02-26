8d110523065cc991600b45bea07c18bf04784aa6
Thefutureseer <danielharrislimos@outlook.com>
2021-02-25 22:44:26 -0800

    Create Header.js

---
+++ react-land/src/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+
+
+export default class Header extends React.Component {
+  render() {
+    return <div className="header-div"><h1>Header</h1></div>
+  }
+}
