de8e64ceeb10c585bdeb40726e998f5f722bf4ec
Thefutureseer <danielharrislimos@outlook.com>
2021-02-27 15:56:56 -0800

    Update Nav.js

--- react-land/src/Nav.js
+++ react-land/src/Nav.js
@@ -4,6 +4,6 @@ import React, { Component } from "react";
 
 export default class Nav extends React.Component {
   render() {
-    return <div className="nav-div"><h1>Nav</h1></div>
+    return <div className="nav-div"><h1>Navbar</h1></div>
   }
 }
